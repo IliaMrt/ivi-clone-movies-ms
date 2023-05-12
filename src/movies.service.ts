b131f778761da555014fc2061de0f4211af67cc5
JcJet <jcjet88@gmail.com>
2023-05-13 00:31:55 +0300

    added comments and files deletion

--- src/movies.service.ts
+++ src/movies.service.ts
@@ -248,6 +248,16 @@ export class MoviesService {
     this.genresClient.emit({ cmd: 'deleteMovieFromGenres' }, id);
 
     this.personsClient.emit({ cmd: 'deleteMovieFromPersons' }, id);
+    
+        //TODO: удаление комментов и файлов фильма. Раскомментить, когда будет добавлен соотв. МС, должно работать
+    //Не претендует на правильность стиля :)
+/*    this.commentsClient.send('deleteCommentsFromEssence', {
+      dto: { essenceTable: 'movies', essenceId: id },
+    });
+
+    this.filesClient.send('deleteFilesFromEssence', {
+      dto: { essenceTable: 'movies', essenceId: id },
+    });*/
 
     return await this.moviesRepository
       .createQueryBuilder()
